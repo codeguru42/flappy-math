17f58ae5e6537c60b65e228cfe277eff66ac141f
Code Apprentice <codeguru42@gmail.com>
2016-02-15 14:41:19 -0700

    Create `GameScreen`

--- core/src/codeguru/flappymath/FlappyMathGame.java
+++ core/src/codeguru/flappymath/FlappyMathGame.java
@@ -2,26 +2,13 @@ package codeguru.flappymath;
 
 import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+import codeguru.flappymath.screens.GameScreen;
 
 public class FlappyMathGame extends Game {
-	SpriteBatch batch;
-	Texture img;
-	
 	@Override
-	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
-	}
-
-	@Override
-	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
+	public void create() {
+		Gdx.app.log("ZBGame", "created");
+		setScreen(new GameScreen());
 	}
 }
