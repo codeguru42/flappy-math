3163c903ea726c23ebdb9601e8736a19a22f2cae
Code Apprentice <codeguru42@gmail.com>
2016-02-15 16:44:37 -0700

    First attempt at scrolling the grass and pipes
    Pipes and grass are rendered based on a positive-y-down direction

--- core/src/codeguru/flappymath/gameworld/GameRenderer.java
+++ core/src/codeguru/flappymath/gameworld/GameRenderer.java
@@ -6,11 +6,19 @@ import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 
+import codeguru.flappymath.gameobjects.Grass;
+import codeguru.flappymath.gameobjects.Pipe;
+import codeguru.flappymath.gameobjects.ScrollHandler;
+import codeguru.flappymath.util.AssetLoader;
+
 public class GameRenderer {
     private final OrthographicCamera cam;
     private final GameWorld world;
     private final ShapeRenderer shapeRenderer = new ShapeRenderer();
     private SpriteBatch batch = new SpriteBatch();
+    private ScrollHandler scroller;
+    private Grass frontGrass, backGrass;
+    private Pipe pipe1, pipe2, pipe3;
 
     public GameRenderer(GameWorld world) {
         cam = new OrthographicCamera();
@@ -19,6 +27,17 @@ public class GameRenderer {
         this.world = world;
         batch.setProjectionMatrix(cam.combined);
         shapeRenderer.setProjectionMatrix(cam.combined);
+
+        initGameObjects();
+    }
+
+    private void initGameObjects() {
+        scroller = world.getScroller();
+        frontGrass = scroller.getFrontGrass();
+        backGrass = scroller.getBackGrass();
+        pipe1 = scroller.getPipe1();
+        pipe2 = scroller.getPipe2();
+        pipe3 = scroller.getPipe3();
     }
 
     public void render() {
@@ -30,5 +49,55 @@ public class GameRenderer {
         shapeRenderer.setColor(147 / 255.0f, 80 / 255.0f, 27 / 255.0f, 1);
         shapeRenderer.rect(0, 0, 136, 52);
         shapeRenderer.end();
+
+        batch.begin();
+        batch.disableBlending();
+        drawGrass();
+        drawPipes();
+        batch.enableBlending();
+        drawSkulls();
+        batch.end();
+    }
+
+    private void drawGrass() {
+        // Draw the grass
+        batch.draw(AssetLoader.grass, frontGrass.getX(), frontGrass.getY(),
+                frontGrass.getWidth(), frontGrass.getHeight());
+        batch.draw(AssetLoader.grass, backGrass.getX(), backGrass.getY(),
+                backGrass.getWidth(), backGrass.getHeight());
+    }
+
+    private void drawSkulls() {
+        batch.draw(AssetLoader.skullUp, pipe1.getX() - 1,
+                pipe1.getY() + pipe1.getHeight() - 14, 24, 14);
+        batch.draw(AssetLoader.skullDown, pipe1.getX() - 1,
+                pipe1.getY() + pipe1.getHeight() + 45, 24, 14);
+
+        batch.draw(AssetLoader.skullUp, pipe2.getX() - 1,
+                pipe2.getY() + pipe2.getHeight() - 14, 24, 14);
+        batch.draw(AssetLoader.skullDown, pipe2.getX() - 1,
+                pipe2.getY() + pipe2.getHeight() + 45, 24, 14);
+
+        batch.draw(AssetLoader.skullUp, pipe3.getX() - 1,
+                pipe3.getY() + pipe3.getHeight() - 14, 24, 14);
+        batch.draw(AssetLoader.skullDown, pipe3.getX() - 1,
+                pipe3.getY() + pipe3.getHeight() + 45, 24, 14);
+    }
+
+    private void drawPipes() {
+        batch.draw(AssetLoader.bar, pipe1.getX(), pipe1.getY(), pipe1.getWidth(),
+                pipe1.getHeight());
+        batch.draw(AssetLoader.bar, pipe1.getX(), pipe1.getY() + pipe1.getHeight() + 45,
+                pipe1.getWidth(), 66 - (pipe1.getHeight() + 45));
+
+        batch.draw(AssetLoader.bar, pipe2.getX(), pipe2.getY(), pipe2.getWidth(),
+                pipe2.getHeight());
+        batch.draw(AssetLoader.bar, pipe2.getX(), pipe2.getY() + pipe2.getHeight() + 45,
+                pipe2.getWidth(), 66 - (pipe2.getHeight() + 45));
+
+        batch.draw(AssetLoader.bar, pipe3.getX(), pipe3.getY(), pipe3.getWidth(),
+                pipe3.getHeight());
+        batch.draw(AssetLoader.bar, pipe3.getX(), pipe3.getY() + pipe3.getHeight() + 45,
+                pipe3.getWidth(), 66 - (pipe3.getHeight() + 45));
     }
 }
