101f683dc3d37c483e8ad33c8419cd201b2f25ca
Code Apprentice <codeguru42@gmail.com>
2016-02-15 16:24:23 -0700

    Draw background sky and dirt rectangles

--- core/src/codeguru/flappymath/gameworld/GameRenderer.java
+++ core/src/codeguru/flappymath/gameworld/GameRenderer.java
@@ -3,35 +3,32 @@ package codeguru.flappymath.gameworld;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
-import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 
 public class GameRenderer {
     private final OrthographicCamera cam;
     private final GameWorld world;
+    private final ShapeRenderer shapeRenderer = new ShapeRenderer();
     private SpriteBatch batch = new SpriteBatch();
-    private Sprite[] sprites;
 
     public GameRenderer(GameWorld world) {
         cam = new OrthographicCamera();
         cam.setToOrtho(false, 136, 204);
 
         this.world = world;
-        sprites = world.getSprites();
         batch.setProjectionMatrix(cam.combined);
+        shapeRenderer.setProjectionMatrix(cam.combined);
     }
 
     public void render() {
-        Gdx.app.log("GameRenderer", "render");
-        // Sets a Color to Fill the Screen with (RGB = 10, 15, 230), Opacity of 1 (100%)
-        Gdx.gl.glClearColor(10 / 255.0f, 15 / 255.0f, 230 / 255.0f, 1f);
-        // Fills the screen with the selected color
+        Gdx.gl.glClearColor(55 / 255.0f, 80 / 255.0f, 100 / 255.0f, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
-        batch.begin();
-        for (int i = 0; i < sprites.length; i++) {
-            batch.draw(sprites[i], 0.0f, i * 20.0f + 5.0f);
-        }
-        batch.end();
+        // Draw Dirt
+        shapeRenderer.begin(ShapeRenderer.ShapeType.Filled);
+        shapeRenderer.setColor(147 / 255.0f, 80 / 255.0f, 27 / 255.0f, 1);
+        shapeRenderer.rect(0, 0, 136, 52);
+        shapeRenderer.end();
     }
 }
