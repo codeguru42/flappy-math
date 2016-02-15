5fb5fc0a1cf629d11586c64be96aca688e0ac3ac
Code Apprentice <codeguru42@gmail.com>
2016-02-15 15:05:40 -0700

    Change window title and size in `DesktopLauncher`

--- desktop/src/codeguru/flappymath/desktop/DesktopLauncher.java
+++ desktop/src/codeguru/flappymath/desktop/DesktopLauncher.java
@@ -5,8 +5,11 @@ import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import codeguru.flappymath.FlappyMathGame;
 
 public class DesktopLauncher {
-	public static void main (String[] arg) {
+	public static void main(String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.title = "Flappy Math";
+		config.width = 272;
+		config.height = 408;
 		new LwjglApplication(new FlappyMathGame(), config);
 	}
 }
