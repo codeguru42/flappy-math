e8013d7207e90583190f2ae2fb9ee8bce31a2424
Code Apprentice <codeguru42@gmail.com>
2016-02-16 18:58:27 -0700

    Fix y-coordinates in `Bird`
    Positive y-direction is up.

--- core/src/codeguru/flappymath/gameobjects/Bird.java
+++ core/src/codeguru/flappymath/gameobjects/Bird.java
@@ -16,23 +16,21 @@ public class Bird {
         this.height = height;
         position = new Vector2(x, y);
         velocity = new Vector2(0, 0);
-        acceleration = new Vector2(0, 460);
+        acceleration = new Vector2(0, -460);
     }
 
     public void update(float delta) {
-
         velocity.add(acceleration.cpy().scl(delta));
 
-        if (velocity.y > 200) {
-            velocity.y = 200;
+        if (velocity.y < -200) {
+            velocity.y = -200;
         }
 
         position.add(velocity.cpy().scl(delta));
-
     }
 
     public void onClick() {
-        velocity.y = -140;
+        velocity.y = 140;
     }
 
     public float getX() {
